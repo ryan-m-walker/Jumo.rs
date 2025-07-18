0b4aae953fe3488195d267bdeefb9621ffca4fa6
ryan-m-walker <ryan.matthew.walker@gmail.com>
2025-07-18 10:09:41 -0600

    cleanup

--- src/app.rs
+++ src/app.rs
@@ -161,66 +161,6 @@ impl App {
     fn render(&mut self) -> Result<(), anyhow::Error> {
         self.terminal
             .draw(|frame| self.renderer.render(frame, &self.state))?;
-
-        //     self.terminal.draw(|frame| {
-        //         let chunks = Layout::default()
-        //             .direction(Direction::Vertical)
-        //             .constraints([Constraint::Length(3), Constraint::Fill(1)].as_ref())
-        //             .split(frame.area());
-        //
-        //         let title = Line::from(" Transcript ".bold());
-        //         let block = Block::bordered()
-        //             .title(title.centered())
-        //             .border_style(Style::new().yellow())
-        //             .border_type(BorderType::Rounded);
-        //
-        //         let header_text = if self.audio_recorder.is_recording() {
-        //             "Recording audio..."
-        //         } else if self.state.is_audio_transcription_running {
-        //             "Transcribing audio..."
-        //         } else if self.state.is_llm_message_running {
-        //             "Sending message to LLM..."
-        //         } else {
-        //             "Press space to start recording audio..."
-        //         };
-        //
-        //         let header = Paragraph::new(header_text).style(Style::new().dim()).block(
-        //             Block::bordered()
-        //                 .border_style(Style::new().yellow())
-        //                 .border_type(BorderType::Rounded),
-        //         );
-        //
-        //         let mut lines = vec![];
-        //
-        //         for line in self.state.transcript.iter() {
-        //             match line {
-        //                 TranscriptLine::TranscriptMessage(line) => {
-        //                     match line.speaker {
-        //                         Speaker::User => {
-        //                             lines.push(Line::from("[User]:").style(Style::new().yellow()));
-        //                         }
-        //                         Speaker::Assistant => {
-        //                             lines.push(Line::from("[Claude]:").style(Style::new().red()));
-        //                         }
-        //                     }
-        //
-        //                     lines.push(Line::from(line.text.clone()));
-        //                 }
-        //                 TranscriptLine::TranscriptError(line) => {
-        //                     lines.push(Line::from(line.text.clone()));
-        //                 }
-        //             }
-        //         }
-        //
-        //         let transcript = Paragraph::new(lines)
-        //             .alignment(Alignment::Left)
-        //             .wrap(Wrap { trim: true })
-        //             .block(block);
-        //
-        //         frame.render_widget(header, chunks[0]);
-        //         frame.render_widget(transcript, chunks[1]);
-        //     })?;
-        //
         Ok(())
     }
 
